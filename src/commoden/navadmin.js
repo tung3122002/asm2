aa21f8e9c5c934b119677073d42ea89fb3cebc17
tung3122002 <tung3122002@gmail.com>
2022-02-21 10:24:24 +0700

    sửa đường dẫn admin

--- src/commoden/navadmin.js
+++ src/commoden/navadmin.js
@@ -38,7 +38,7 @@ const navadmin = {
                 <div class="hidden sm:block sm:ml-6">
                   <div class="flex space-x-4">
                     <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <a href="/#/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Trang chủ</a>
+                    <a href="/" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" aria-current="page">Trang chủ</a>
         
                     <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Team</a>
         
